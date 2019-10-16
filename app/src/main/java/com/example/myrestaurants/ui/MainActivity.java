ec365d87a21ad849b712a3796966548b69074891
GraceUmutesi <tesigrace98@gmail.com>
2019-10-16 18:00:12 +0200

    monday changes

--- app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myrestaurants;
+package com.example.myrestaurants.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.myrestaurants.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
